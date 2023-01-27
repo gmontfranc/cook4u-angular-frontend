f2668b5e1c2d110522175da324766edf92511d31
Francisco <Montilla2808@gmail.com>
2023-01-27 17:53:34 +0100

    Implementing login functionnality

--- src/app/connection/signin/signin.component.ts
+++ src/app/connection/signin/signin.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent {
+  username: string = ""
+  password: string = ""
 
+  constructor(private loginService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.loginService.login(this.username, this.password)
+      .subscribe(
+        (data) => {
+          this.router.navigate(['/home']);
+        },
+        (error) => {
+          console.log(error);
+        });
+  }
 }
+
