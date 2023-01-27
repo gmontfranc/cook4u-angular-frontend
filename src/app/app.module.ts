f59ea3982cdd29e26944f733bff77a0a1a22d861
Francisco <Montilla2808@gmail.com>
2023-01-27 17:30:19 +0100

    Adding basic packages and preparation for homepage and login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './utils/navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar'
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { HomepageComponent } from './component/homepage/homepage.component';
+import { SigninComponent } from './connection/signin/signin.component';
+import { SignoutComponent } from './connection/signout/signout.component';
+import { SignupComponent } from './connection/signup/signup.component'; 
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent,
+    SigninComponent,
+    SignoutComponent,
+    SignupComponent,
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
