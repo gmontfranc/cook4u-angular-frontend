c28b0f93322391ab683482adef4db16edc7fdcb5
Francisco <Montilla2808@gmail.com>
2023-01-28 17:15:39 +0100

    Completion of sign in feature without token retrieval

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,36 +1,31 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { CookieService } from 'ngx-cookie-service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AuthService {
 
-  private apiUrl = 'http://localhost:8080/api/auth';
+  private BASE_AUTH_URL = 'http://localhost:8080/api/auth/';
 
   constructor(private http: HttpClient, private cookieService: CookieService) { }
 
-  login(username: string, password: string): Observable<any> {
+  login(email: string, password: string) {
     const httpOptions = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' })
     };
 
-    const body = JSON.stringify({ username, password });
+    const body = JSON.stringify({ email, password });
 
-    return this.http.post<any>(this.apiUrl, body, httpOptions)
-      .pipe(
-        map((data) => {
-          if (data && data.token) {
-            this.cookieService.set('jwt', data.token);
-            return true;
-          }
-          //error: err => alert("Combo username password is wrong!"),
-          return false;
-        })
-      );
+    this.http.post<any>(this.BASE_AUTH_URL + 'login', body, httpOptions).subscribe( =>{
+      next:() => {
+        console.log("#FR HEADERS: ")
+        //this.cookieService.set('jwt', );
+        //this.cookieService.set('firstname', data.token);
+      }
+    });
+      
   }
   
 }
