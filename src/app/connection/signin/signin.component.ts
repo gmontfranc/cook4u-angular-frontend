8575c396565c544b2a7f2a68429eb7e9e9e4c7c6
Montilla Francisco <fj.montilla@soprasteria.com>
2023-02-06 11:46:20 +0100

    Applying signin form

--- src/app/connection/signin/signin.component.ts
+++ src/app/connection/signin/signin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service'
 import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -8,13 +9,23 @@ import { Router } from '@angular/router';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
-  email: string = ""
-  password: string = ""
+  email: string;
+  password: string;
+  signin: FormGroup;
+  hide = true;
 
-  constructor(private auth: AuthService, private router: Router) { }
+  constructor(private auth: AuthService, private router: Router, private formHelper: FormBuilder) { }
 
-  ngOnInit() {
+  ngOnInit(): void  {
+    this.signin = this.formHelper.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required]
+
+    });
   }
+
+  get emailInput() { return this.signin.get('email'); }
+  get passwordInput() { return this.signin.get('password'); }  
 
   login() {
     this.auth.login(this.email, this.password);
